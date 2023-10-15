43b304795e329353b8981901e5f1e021d6cd3a89
golfguy1984 <stephenrwalters@gmail.com>
2023-10-15 07:59:04 -0400

    a couple updates

--- src/App.jsx
+++ src/App.jsx
@@ -50,7 +50,7 @@ export default function App() {
             <button className='header-btn'>Get In Touch</button>
         </div>
             <div className="hero-image">
-                <img src="/Hero.png"/>
+                <img src="/Hero2.png"/>
             </div>
         </div>
     </header>
--- utils/content.jsx
+++ utils/content.jsx
@@ -16,27 +16,60 @@ export const accordionData = [
   },
   {     company: 'TTEC',
         title: 'Channel Marketing Manager',
-        dates: 'August 2022 - Apil 2023',
-        content: `Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quis sapiente
-        laborum cupiditate possimus labore, hic temporibus velit dicta earum
-        suscipit commodi eum enim atque at? Et perspiciatis dolore iure
-        voluptatem.`
+        dates: 'June 2021 - August 2022',
+        content: <ul>
+        <li>Develop business and marketing plans for assigned partners while aligning with sales, executive sponsors, and industry leaders.</li>
+        
+        <li> Drove the creation of sales enablement collateral, marketing collateral, event strategy, and digital campaigns to support joint initiatives.</li>
+        
+        <li>Collaborated with Partner Account Managers to drive sales enablement and marketing communication initiatives.</li>
+
+        <li>Work with the Partner Development Managers to design sales acceleration programs and initiatives that enhance collaboration, innovation, and rapport between sales teams.</li>
+        </ul>
   },
   {     company: 'LivePerson',
         title: 'Partner Marketing Manager',
-        dates: 'August 2022 - Apil 2023',
-        content: `Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quis sapiente
-        laborum cupiditate possimus labore, hic temporibus velit dicta earum
-        suscipit commodi eum enim atque at? Et perspiciatis dolore iure
-        voluptatem.`
+        dates: 'January 2019 - September 2020',
+        content: <ul>
+        <li>Developed marketing strategies, plans, and programs to drive joint demand generation with prioritized partners.</li>
+        
+        <li> Performed market, customer, partner, and competitive research in order to develop joint value props, positioning, and narratives that differentiated our offerings from the rest of the market — and that make it easy for salespeople and partners to tell our story.</li>
+        
+        <li>Collaborated with Partner Account Managers to drive sales enablement and marketing communication initiatives.</li>
+        
+        <li>Supported revenue growth by working with internal and external stakeholders to develop enablement tools and content. Fostered relationships with partners' marketing and sales teams to create mutually beneficial activity that drove pipeline, accelerated deal velocity, increased win rates, and supported customer retention.</li>
+        </ul>
   },
   {     company: 'Profisee',
+        title: 'Sales/Partner Enablement Manager',
+        dates: 'September 2016 - January 2019',
+        content: <ul>
+        <li>Created and managed Profisee’s “Award Winning” Academy to create industry-leading on-demand training for customers, partners, and internal workforce. Leveraged facilities to create thought leadership content pieces used in marketing campaigns.</li>
+        
+        <li> Developed partner & sales enablement curriculum and associated plans to collect feedback, optimize and grow the overall coursework in the curriculum.</li>
+        
+        <li>Provided innovation and creativity around sales & partner enablement efforts to drive the most impact.</li>
+        
+        <li>Collaborated with, VP of product,  partner solutions teams, and partner marketing to support GTM and enablement events.</li>
+
+        <li>Produced, enhanced, and facilitated virtual and in-person sales onboarding programs to immerse partners in Profisse’s products, capabilities and solutions.</li>
+
+        <li>Reviewed, monitored, and updated the efficacy of online Learning Modules.</li>
+        </ul>
+  },
+
+  {     company: 'MarketSource',
         title: 'Creative Director',
-        dates: 'August 2022 - Apil 2023',
-        content: `Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quis sapiente
-        laborum cupiditate possimus labore, hic temporibus velit dicta earum
-        suscipit commodi eum enim atque at? Et perspiciatis dolore iure
-        voluptatem.`
+        dates: 'September 2009 - September 2016',
+        content: <ul>
+        <li>Identified and initiated the launch of multimedia services within our organization.</li>
+        
+        <li> Created graphics, filmed content and edited video material for all e-learning, client-facing content and corporate back office projects.</li>
+        
+        <li>Managed creative projects from start to finish. Collaborate with internal clients to evaluate individual project needs, gather business requirements, and conceptualize the final product.</li>
+        
+        <li>Created training and marketing material for clients such as Target, Apple, LG, Whirlpool, Ford, and other retail/B2B clients.</li>
+        </ul>
   },
   ]
   
@@ -49,7 +82,7 @@ export const accordionData = [
     bgColor: "#27CEC5"
 },
 {
-    name: "MEME GENERATOR",
+    name: "QUIZZICAL",
     image: "tenzies",
     code: "www.espn.com",
     preview: "www.cnn.com",
@@ -72,8 +105,8 @@ export const accordionData = [
 {
     name: "UNIT CONVERTER",
     image: "./unit-converter.png",
-    code: "www.espn.com",
-    preview: "https://github.com/golfguy1984/unit-converter",
+    code: "https://github.com/golfguy1984/unit-converter",
+    preview: "https://incandescent-tiramisu-6d2b8f.netlify.app",
     bgColor: "#27CEC5"
 },
 {
@@ -87,8 +120,8 @@ export const accordionData = [
 {
     name: "HIGH FIVE HUB",
     image: "./high-five.png",
-    code: "www.espn.com",
-    preview: "www.cnn.com",
+    code: "https://github.com/golfguy1984/high-five-hub",
+    preview: "https://frolicking-torrone-617e0b.netlify.app/",
     bgColor: "#27CEC5"
 },
 {
