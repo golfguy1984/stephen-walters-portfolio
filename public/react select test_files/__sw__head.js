8019732f427370fde27e2b4a4a623e9089ef3e84
golfguy1984 <stephenrwalters@gmail.com>
2023-11-29 09:50:00 -0500

    removed test fiiles

--- public/react select test_files/__sw__head.js
+++
@@ -1,3 +0,0 @@
-
-(()=>{globalThis.esmsInitOptions={shimMode:!0,fetch:async function(o){let e={Accept:"application/javascript"};return await window.fetch(o,{headers:e})},onerror:function(o){throw o},revokeBlobURLs:!1};globalThis.process={env:{NODE_ENV:"production"}};})();
-//__FOOT__
